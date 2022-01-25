b157d03aa36cb018c74dde3e3e3171a9984c7d64
georgesdsk <sadachok99@gmail.com>
2022-01-25 11:47:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/usersspprimerproyecto/User.kt
@@ -0,0 +1,3 @@
+package com.example.usersspprimerproyecto
+
+data class User(val id: Int, var name: String = "Pepito", var lastName: String ="Perez", var url: String)
---
+++ app/src/main/java/com/example/usersspprimerproyecto/UserAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.usersspprimerproyecto
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class UserAdapter(private val users: List<User>) {
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder()
+}
