fb2ba2ee1012917fa7d30b661c8b28e0e924b7b0
georgesdsk <sadachok99@gmail.com>
2022-01-31 11:45:42 +0100

    diaaablo

--- app/src/main/java/com/example/usersspprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/example/usersspprimerproyecto/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.usersspprimerproyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.BoringLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.usersspprimerproyecto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var lineaLayoutManager: RecyclerView.LayoutManager
+    private lateinit var binding: ActivityMainBinding // se declara el binding hacia la futura vista
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)// se infla el binding
+        setContentView(binding.root) // y se asigna al binding hacia esta ACTIVIDAD
+
+        userAdapter = UserAdapter(mutableListOf()) // inicializacion de un listado, esta vacio
+        lineaLayoutManager = LinearLayoutManager(this)
+
+        binding.recyclerView.apply {
+            layoutManager = lineaLayoutManager // el layout
+            adapter = userAdapter // el adaptador
+        }
+
     }
+
+    private fun getUsers(): MutableList<User>{
+        val users: MutableList<User>()
+
+    }
+
 }
--- app/src/main/java/com/example/usersspprimerproyecto/UserAdapter.kt
+++ app/src/main/java/com/example/usersspprimerproyecto/UserAdapter.kt
@@ -1,9 +1,45 @@
 package com.example.usersspprimerproyecto
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.usersspprimerproyecto.databinding.ItemUserBinding
 
-class UserAdapter(private val users: List<User>) {
+class UserAdapter(private val users: List<User>) : RecyclerView.Adapter<UserAdapter.ViewHolder>() {
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder()
+    private lateinit var context: Context /*la varibla se va a inicializar despues*/
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        val view = LayoutInflater.from(context).inflate(R.layout.item_user, parent, false)
+
+        return ViewHolder(view) // se devuelve el viewHolder con el binding hacia la vista del ITEM
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = users.get(position)
+
+        with(holder){ // se va a alimentar cada propiedad de la vista con los datos de cada user
+            binding.tvOrder.text = user.id.toString()
+            binding.tvName.text = user.name
+
+        }
+    }
+
+
+
+    override fun getItemCount(): Int = users.size
+
+
+
+    // definicion de la clase
+    inner class ViewHolder(view : View ): RecyclerView.ViewHolder(view){
+        val binding = ItemUserBinding.bind(view) // no entiendo que es el itemUser
+    }
+
 }
